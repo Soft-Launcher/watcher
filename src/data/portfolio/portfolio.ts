e6177d8d74c6623e6c02919de6d6738046051352
apveliz <apvelizs@utn.edu.ec>
2024-04-09 21:38:37 -0500

    Feat: Advance portfolio component

---
+++ src/data/portfolio/portfolio.ts
@@ -0,0 +1,44 @@
+import type {Portfolio} from '../../interfaces/portfolio';
+
+export const projectsPortfolio: Portfolio[] = [
+    {
+        id: 1,
+        name: 'GAD Ibarra',
+        category: 'mobile',
+        image: '../../../public/assets/portfolio/ibarra.jpg',
+        caption: '',
+        actividad: '',
+        client: '',
+        services: 'Aplicación móvil',
+    },
+    {
+        id: 2,
+        name: 'GAD Ibarra web',
+        category: 'web',
+        image: '',
+        caption: '',
+        client: '',
+        actividad: '',
+        services: 'Aplicacion web',
+    },
+    {
+        id: 3,
+        name: 'Cultural',
+        category: 'mobile',
+        image: '',
+        caption: '',
+        client: '',
+        actividad: '',
+        services: 'Aplicación móvil',
+    },
+    {
+        id: 4,
+        name: 'GAD Cotacachi',
+        category: 'web',
+        image: '',
+        caption: '',
+        client: '',
+        actividad: '',
+        services: 'Sitio web',
+    },
+];
