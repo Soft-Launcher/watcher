4cdca5041b8a217c58d2ada4faada59af7aa5147
apveliz <apvelizs@utn.edu.ec>
2024-04-13 10:55:47 -0500

    Feat: Complete contact

--- src/components/general/CustomButton.tsx
+++ src/components/general/CustomButton.tsx
@@ -1,19 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 type Props = {
-  title:string,
-  onClick?:()=>void
-}
+    title: string;
+    onClick?: () => void;
+    type?: 'submit' | 'reset' | 'button' | undefined;
+};
 
-const CustomButton = ({title,onClick}: Props) => {
-  return (
-    <button onClick={onClick}
-    className='inline-block bg-primary text-secondary outline-none border-none font-body py-2 px-4 rounded-md transition-all duration-200 ease-in-out relative hover:scale-90'
-
-    >
-      {title}
-    </button>
-  )
-}
+const CustomButton = ({title, onClick, type}: Props) => {
+    return (
+        <button
+            type={type}
+            onClick={onClick}
+            className="cursor-pointer inline-block bg-primary text-secondary outline-none border-none font-body py-2 px-4 rounded-md transition-all duration-200 ease-in-out relative hover:scale-90"
+        >
+            {title}
+        </button>
+    );
+};
 
 export default CustomButton;
--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -1,12 +1,95 @@
+import CustomInput from '../general/CustomInput';
+import CustomButton from '../general/CustomButton';
+import {useState} from 'react';
+import MenuItems from '../navigation/MenuItems';
 
 const Contact = () => {
-  return (
-    <>
-      <section id="contact" className="w-full min-h-screen">
-        <h1>CONTACTO</h1>
-      </section>
-    </>
-  );
+    const scrollTo = (id: string) => {
+        const element = document.getElementById(id);
+        if (element) {
+            const navigationHeight =
+                document.getElementById('navigation')?.offsetHeight;
+            window.scrollTo({
+                top: element.offsetTop - (navigationHeight || 0),
+                behavior: 'smooth', // Desplazamiento suave
+            });
+        }
+    };
+
+    const handleSubmit = (event: any) => {
+        event.preventDefault();
+        console.log('Valor del input:');
+    };
+
+    return (
+        <>
+            <footer
+                id="contact"
+                className="w-full min-h-96 bg-secondary py-12 px-20 flex"
+            >
+                <div className="w-5/12">
+                    <img
+                        src="./logo-white.png"
+                        alt="LOGO"
+                        width={300}
+                        height={300}
+                    />
+                </div>
+                <div className="w-5/12 flex flex-col items-center">
+                    <h4 className="font-body font-bold text-white text-2xl pb-12">
+                        NAVEGACION
+                    </h4>
+                    <nav>
+                        <ul className="flex flex-col justify-center items-center list-none gap-2">
+                            <MenuItems
+                                onclick={() => scrollTo('home')}
+                                title="INICIO"
+                            />
+                            <MenuItems
+                                onclick={() => scrollTo('about')}
+                                title="NOSOTROS"
+                            />
+                            <MenuItems
+                                onclick={() => scrollTo('portfolio')}
+                                title="PORTAFOLIO"
+                            />
+                            <MenuItems
+                                onclick={() => scrollTo('team')}
+                                title="EQUIPO"
+                            />
+                            <MenuItems
+                                onclick={() => scrollTo('testimonials')}
+                                title="TESTIMONIOS"
+                            />
+                            <MenuItems
+                                onclick={() => scrollTo('contact')}
+                                title="CONTACTO"
+                            />
+                        </ul>
+                    </nav>
+                </div>
+                <div className="w-8/12 flex flex-col items-center">
+                    <h4 className="font-body font-bold text-white text-2xl pb-12">
+                        PROPUESTAS
+                    </h4>
+                    <form
+                        onSubmit={handleSubmit}
+                        className="flex flex-col items-center w-full"
+                    >
+                        <CustomInput
+                            type="text"
+                            placeholder="Ingrese su correo"
+                        />
+                        <CustomInput
+                            type="text"
+                            placeholder="Envía tu propuesta"
+                        />
+                        <CustomButton type="submit" title="ENVIAR" />
+                    </form>
+                </div>
+            </footer>
+        </>
+    );
 };
 
 export default Contact;
