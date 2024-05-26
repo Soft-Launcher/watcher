59afcd0c9cc1ba29b33023ded130033942715ea1
Miguel Rodríguez <miguel_rodriguez@disroot.org>
2024-05-26 15:59:08 -0500

    Restablished payload project

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,27 +1,27 @@
-import express from 'express';
-import payload from 'payload';
+import express from 'express'
+import payload from 'payload'
 
-require('dotenv').config();
-const app = express();
+require('dotenv').config()
+const app = express()
 
 // Redirect root to Admin panel
 app.get('/', (_, res) => {
-    res.redirect(process.env.BASEPATH || '/admin');
-});
+  res.redirect('/admin')
+})
 
 const start = async () => {
-    // Initialize Payload
-    await payload.init({
-        secret: process.env.PAYLOAD_SECRET,
-        express: app,
-        onInit: async () => {
-            payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
-        },
-    });
+  // Initialize Payload
+  await payload.init({
+    secret: process.env.PAYLOAD_SECRET,
+    express: app,
+    onInit: async () => {
+      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
+    },
+  })
 
-    // Add your own express routes here
+  // Add your own express routes here
 
-    app.listen(process.env.PORT || 3000);
-};
+  app.listen(3000)
+}
 
-start();
+start()
