d37478fc7e1c00484cc31d6c1a5a23b407157549
Miguel Rodríguez <miguelrodrii@outlook.com>
2025-01-08 19:10:01 -0500

    8.1.25

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,22 +8,22 @@ const port = process.env.PORT || 3000;
 
 // Redirect root to Admin panel
 app.get('/', (_, res) => {
-    res.redirect(`${basePath || '/admin'}/ui`);
+  res.redirect(`${basePath || '/admin'}/ui`);
 });
 
 const start = async () => {
-    // Initialize Payload
-    await payload.init({
-        secret: process.env.PAYLOAD_SECRET,
-        express: app,
-        onInit: async () => {
-            payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
-        },
-    });
+  // Initialize Payload
+  await payload.init({
+    secret: process.env.PAYLOAD_SECRET,
+    express: app,
+    onInit: async () => {
+      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`);
+    },
+  });
 
-    // Add your own express routes here
+  // Add your own express routes here
 
-    app.listen(port);
+  app.listen(port);
 };
 
 start();
